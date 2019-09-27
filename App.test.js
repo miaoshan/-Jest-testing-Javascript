3dd69911ad569c703ef7808fb3b24a08cc448b67
miaoshan <minnieshan@gmail.com>
2019-09-27 23:33:37 +0100

    Add files via upload

--- App.test.js
+++ App.test.js
@@ -1,3 +1,12 @@
+import {hello} from './App';
+
+describe('hello', ()=>{
+    it('should output hello', ()=>{
+        expect(hello()).toBe('hello');
+    })
+})
+
+
 test('Fake Test', ()=> {
 
     expect(true).toBeTruthy

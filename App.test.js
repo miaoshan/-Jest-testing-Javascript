2224d540b3caca45d72f9ecd0f77016d0f1a8e7c
miaoshan <minnieshan@gmail.com>
2019-09-27 23:56:59 +0100

    Add files via upload

--- App.test.js
+++ App.test.js
@@ -1,13 +1,38 @@
-import {hello} from './App';
+import {
+    hello,
+    add
+} from './App';
 
-describe('hello', ()=>{
-    it('should output hello', ()=>{
+describe('hello', () => {
+    it('should output hello', () => {
         expect(hello()).toBe('hello');
     })
 })
 
+describe('add', () => {
+    it('should add two numbers', () => {
+        expect(add(1, 2)).toBe(3);
+        expect(add(-2, 2)).toBe(0);
+        expect(add(0, 2)).toBe(2);
+        expect(add(-3, -2)).toBe(-5);
+    });
+    it('shoudl not add strings', () => {
+        expect(add(2, '2')).toBe(null);
+    });
+    it('shoudl not add objects', () => {
+        expect(add(2, {})).toBe(null);
+    });
+    it('shoudl not add arrays', () => {
+        expect(add(2, [])).toBe(null);
+    });
+});
 
-test('Fake Test', ()=> {
+
+
+
+
+
+test('Fake Test', () => {
 
     expect(true).toBeTruthy
 })

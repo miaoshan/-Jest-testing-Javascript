d4c06bcc8f947c2c29018677adca23af91dc0457
miaoshan <minnieshan@gmail.com>
2019-09-28 00:20:53 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -8,5 +8,12 @@ export const add=(x,y)=>{
 }
 
 export const removeSNames = names =>{
-    return null;
+   let newNames = names; 
+   names.forEach(name=>{
+       if(name.toLowerCase().charAt(0)=='s'){
+           const index= newNames.indexOf(name);
+           newNames.splice(index, 1);
+       }
+   })
+   return newNames;
 }

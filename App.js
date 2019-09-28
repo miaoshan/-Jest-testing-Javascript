c4ccffca570cf54e6612a55b2c00301a06c04127
miaoshan <minnieshan@gmail.com>
2019-09-28 15:17:07 +0100

    Add files via upload
    add integration test

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-export const hello =()=> 'hello';
+export const hello = () => 'hello';
 
-export const add=(x,y)=>{
-    if(typeof x != 'number' || typeof y != 'number'){
-        return null
-    }
-    return x+y;
+export const removeSNames = names => {
+    let newNames = names;
+    names.forEach(name => {
+        if (name.toLowerCase().charAt(0) == 's') {
+            const index = newNames.indexOf(name);
+            newNames.splice(index, 1);
+        }
+    })
+    return newNames;
 }
 
-export const removeSNames = names =>{
-   let newNames = names; 
-   names.forEach(name=>{
-       if(name.toLowerCase().charAt(0)=='s'){
-           const index= newNames.indexOf(name);
-           newNames.splice(index, 1);
-       }
-   })
-   return newNames;
+
+export const add = (x, y) => {
+    if (typeof x != 'number' || typeof y != 'number') {
+        return null;
+    }
+    return x + y;
+}
+
+// export const add=(x,y)=> x+y;
+
+export const total =(deliveryCost, subTotal)=>{
+    return "£" + add(deliveryCost, subTotal);
 }
--- App.test.js
+++ App.test.js
@@ -1,28 +1,10 @@
-import {hello, add, removeSNames} from './App';
+import {hello, add, removeSNames, total} from './App';
 
 describe('hello', () => {
     it('should output hello', () => {
         expect(hello()).toBe('hello');
     })
 })
-
-describe('add', () => {
-    it('should add two numbers', () => {
-        expect(add(1, 2)).toBe(3);
-        expect(add(-2, 2)).toBe(0);
-        expect(add(0, 2)).toBe(2);
-        expect(add(-3, -2)).toBe(-5);
-    });
-    it('shoudl not add strings', () => {
-        expect(add(2, '2')).toBe(null);
-    });
-    it('shoudl not add objects', () => {
-        expect(add(2, {})).toBe(null);
-    });
-    it('shoudl not add arrays', () => {
-        expect(add(2, [])).toBe(null);
-    });
-});
 
 describe('removeSNames', ()=>{
     it('should remove all s names', ()=>{
@@ -43,11 +25,32 @@ describe('removeSNames', ()=>{
     });
 })
 
-
-
-
-
 test('Fake Test', () => {
 
     expect(true).toBeTruthy
 })
+
+// unit test 
+describe('add', () => {
+    it('should add two numbers', () => {
+        expect(add(1, 2)).toBe(3);
+        expect(add(-2, 2)).toBe(0);
+        expect(add(0, 2)).toBe(2);
+        expect(add(-3, -2)).toBe(-5);
+    });
+    it('shoudl not add strings', () => {
+        expect(add(2, '2')).toBe(null);
+    });
+    it('shoudl not add objects', () => {
+        expect(add(2, {})).toBe(null);
+    });
+    it('shoudl not add arrays', () => {
+        expect(add(2, [])).toBe(null);
+    });
+});
+
+
+// integration test. not only testing the functionality of total, also testing the functionality of add.
+ test('total', ()=>{
+    expect(total(5,29)).toBe('£34');
+ })
